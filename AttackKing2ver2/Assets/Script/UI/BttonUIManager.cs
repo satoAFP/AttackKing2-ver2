183a13c61728c2d514b875f5bd93c4bad98d6e9b
satoAFP <72423443+satoAFP@users.noreply.github.com>
2022-06-21 01:50:36 +0900

    UIの作成中

--- AttackKing2ver2/Assets/Script/UI/BttonUIManager.cs
+++ AttackKing2ver2/Assets/Script/UI/BttonUIManager.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class BttonUIManager : MonoBehaviour
 {
-    
+    [SerializeField, Header("チェスト")] GameObject chest;
+
+
+    public void ChestOpen()
+    {
+        if (!chest.activeSelf)
+            chest.SetActive(true);
+        else
+            chest.SetActive(false);
+    }
+
 }
--- AttackKing2ver2/Assets/Script/UI/OnUIManager.cs
+++ AttackKing2ver2/Assets/Script/UI/OnUIManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class OnUIManager : MonoBehaviour
 {
+    [SerializeField] Vector3 mouse;
 
+    [SerializeField,Header("ステータス表示用パネル")] GameObject StatusPanel;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,11 @@ public class OnUIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mouse = Input.mousePosition;
+
+        if (mouse.x > 0 && mouse.x < 254 && mouse.y < 576 && mouse.y > 540)
+            StatusPanel.SetActive(true);
+        else
+            StatusPanel.SetActive(false);
     }
 }
